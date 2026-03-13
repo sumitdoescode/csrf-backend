58b1ae2ffa477bd838cd048db478e07047389e98
sumitdoescode <sumit.does.code@gmail.com>
2026-03-13 22:21:32 +0530

    hellyeah

--- db/db.ts
+++ db/db.ts
@@ -2,10 +2,15 @@ import { connect } from "mongoose";
 
 export const connectDB = async () => {
     try {
-        const { connection } = await connect(process.env.MONGO_URI!);
+        const mongoUri = process.env.MONGODB_URI;
+        if (!mongoUri) {
+            throw new Error("Missing MONGODB_URI in environment");
+        }
+
+        const { connection } = await connect(mongoUri);
         console.log("MongoDB connected", connection.host);
     } catch (error) {
-        process.exit(1);
         console.log(error);
+        process.exit(1);
     }
 };
